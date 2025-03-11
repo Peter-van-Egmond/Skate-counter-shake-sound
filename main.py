e37c30711f7ad7f63151442bd089917524dd5450
Peter van Egmond <77121927+Peter-van-Egmond@users.noreply.github.com>
2025-03-11 16:08:04 +0100

    main.blocks, main.ts, main.py bijwerken

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 Rondes = 0
 
-def on_button_pressed_a():
+def on_gesture_screen_down():
     global Rondes
-    Rondes = 0
+    Rondes += 1
+    basic.show_string("" + str(Rondes))
+    music.play(music.string_playable("- - - C5 - - - - ", 120),
+        music.PlaybackMode.IN_BACKGROUND)
+    basic.pause(100)
+input.on_gesture(Gesture.SCREEN_DOWN, on_gesture_screen_down)
+
+def on_button_pressed_a():
+    basic.show_string("" + str(Rondes))
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_button_pressed_ab():
-    basic.show_string("" + str(Rondes))
+    global Rondes
+    Rondes = 0
 input.on_button_pressed(Button.AB, on_button_pressed_ab)
 
 def on_button_pressed_b():
     global Rondes
     Rondes += 1
+    music.play(music.string_playable("- - - C5 - - - - ", 120),
+        music.PlaybackMode.IN_BACKGROUND)
     basic.show_string("" + str(Rondes))
+    basic.pause(100)
 input.on_button_pressed(Button.B, on_button_pressed_b)
--- main.ts
+++ main.ts
@@ -2,6 +2,7 @@ let Rondes = 0
 input.onGesture(Gesture.ScreenDown, function () {
     Rondes += 1
     basic.showString("" + Rondes)
+    music.play(music.stringPlayable("- - - C5 - - - - ", 120), music.PlaybackMode.InBackground)
     basic.pause(100)
 })
 input.onButtonPressed(Button.A, function () {
@@ -12,5 +13,7 @@ input.onButtonPressed(Button.AB, function () {
 })
 input.onButtonPressed(Button.B, function () {
     Rondes += 1
+    music.play(music.stringPlayable("- - - C5 - - - - ", 120), music.PlaybackMode.InBackground)
     basic.showString("" + Rondes)
+    basic.pause(100)
 })
